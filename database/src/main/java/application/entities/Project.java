72544b26647058f6a13c535ae358894028aa1487
Lucanonym <lucaleon@pallo.de>
2022-12-30 19:06:55 +0100

    single imports

--- database/src/main/java/application/entities/Project.java
+++ database/src/main/java/application/entities/Project.java
@@ -1,7 +1,11 @@
 package application.entities;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
